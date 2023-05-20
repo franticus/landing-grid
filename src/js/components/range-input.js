35cdbb3d93ae5618da7e4d7ac79577994c4dc4e9
S.Zelensky <zelenstein@gmail.com>
2023-05-20 20:52:59 +0300

    Deploy to gh-pages

--- src/js/components/range-input.js
+++ src/js/components/range-input.js
@@ -2,6 +2,5 @@ import vars from "../_vars";
 
 vars.$rangeInput.addEventListener('input', function () {
   const value = this.value;
-  console.log('value:', value)
   vars.$rangeOutput.textContent = value + '%';
 });
